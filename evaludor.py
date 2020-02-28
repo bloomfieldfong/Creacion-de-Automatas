a59470b760d53498f026c64073b497921c19d076
Michelle Bloomfield <blo16803@uvg.edu.gt>
2020-02-27 18:12:46 -0600

    changes

--- evaludor.py
+++ evaludor.py
@@ -1,59 +1,127 @@
-## evaluador de expresiones
-## 100 x 25
+##########################################
+##  Universidad del Valle de Guatemala  ##
+##  Diseño de Lenguajes de programacion ##
+##  Michelle Bloomfield Fong            ##
+##  Carne: 16803                        ##
+##########################################
 
 
-def evaluate(digito1, digito2, operador):
-    if(operador == "+"):
-        return digito1+digito2
-    if(operador == "-"):
-        return digito1-digito2
-    if(operador == "*"):
-        return digito1*digito2
-    if(operador == "/"):
-        return digito1/digito2
+# nos definie la precedencia entre operadores
+def precedencia(op): 
+    if op == '*' or op == '/': return 2
+    if op == '|' or op == '-': return 1
+    if op == "(": return 3
+    else: return 0
+
+
+def listToStr(cadena):
+    nueva = ''
+    for i in range(0,len(cadena)):
+        nueva = nueva + str(cadena[i])         
+    return nueva 
     
-    
-def principal(token):
-    lista = []
-    operadores = []
-    for i in range(0,len(token)):
-        if token[i] == "*" or token[i] == "/" or token[i] == "+" or token[i] == "-":
-            operadores.append(token[i])
-        else:
-            
-            lista.append(token[i])
-    return lista, operadores
-
-
-def jerarquia(lista):
-    jerarquia = []
-    for i in range(0,len(lista)):
-        if lista[i] == "*" or lista[i] == "/":
-            jerarquia.append("1")
-        else:
-            jerarquia.append("0")
-    return jerarquia
-
+def expandir(cadena):
+  stack = []
+  variable = []
+  i = 0
+  catn_parentesis = 1
+  while i < len(cadena):
+    variable.append(cadena[i])
+    if cadena[i] == "?":
+      if cadena[-2] == ")":
+        parentesis = []
+        x = i
+        while cadena[x] != "(":
+          catn_parentesis += 1
+          parentesis.append(cadena[x])
+          variable.pop()
+          x-= 1
           
+        x = parentesis[::-1]
+        x.pop()
+        y  =  "("+listToStr(x)+"|e)"
+        variable.append(y)
+      else: 
+        variable[i-1] = "("+variable[-2]+"|e)"
+    i+=1
+  print(listToStr(variable))
 
-x = principal("2*1+2")
-print(x)
-print(jerarquia(x[1]))
+    
+#Cambia de infix a postdfix
+def infix_to_postfix(cadena):
+  
+  #operadores validos
+  op_validos = ["|", "*",]
+  ##stack
+  stack = []
+  ##output
+  valores = []
+  i = 0
 
-def hola(lista):
-    orden = principal(lista)
-    jerarquia = jerarquia(x[1])
-    respuesta = []
-    for i in range(0, len(jerarquia)):
-        if jerarquia[i] == 1:
-            if orden[1][i] == "*":
-                orden[0][i] = orden[1][i] * orden[1][i+1] 
-           
+  while i < len(cadena):
 
-hola("2*1+2")
-        
+    # Si tenemos un parantesis abierto entonces se ira al stack
+    if cadena[i] == "(":
+      stack.append(cadena[i])
+    
+    # Si es un parentesis cerrado entonces saca todo del stack
+    # hasta encontrar un parentesis abierto
+    elif cadena[i] == ")":
+      x = len(stack) - 1
+      while stack[x] != "(":
+        #se agrega el ultimo valor de stack a mi output
+        valores.append(stack[x])
+        #se realiza pop del ultimo numero
+        stack.pop()
+        x-=1
+      # elimina el parentesis abierto
+      stack.pop()
+      
+    ##Si es un operador entonces se agrega al stack (depende de presedencia)
+    elif cadena[i] in op_validos:
+      #Revisa si el stack no esta vacio
+      if len(stack) == 0: 
+        stack.append(cadena[i])
+      else:
+        #Si el operador anterior no es un parentesis
+        if stack[-1] != '(':
+          #Si la precendencia de la cadena es menor a la del stack
+          if precedencia(cadena[i]) < precedencia(stack[-1]):
+            #Si en el stack solo hay un objeto
+            z = len(stack) - 1
+            if z != 0:
+                #Se saca todo del stack y se agrega el valor nuevo al stack
+                #se saca todos menos el parentesis
+                while stack[z] != '(':
+                  valores.append(stack[-1])
+                  stack.pop()
+                  z -= 1
+                stack.append(cadena[i])
+            else:
+                #Se saca lo que hay y se agrega el valor nuevo  
+                valores.append(stack[-1])
+                stack.pop()
+                stack.append(cadena[i])
+          #si la precedencia es mayor se agrega al stack
+          elif precedencia(cadena[i]) >= precedencia(stack[-1]): 
+            stack.append(cadena[i])
+        else:
+          #Si no hay nada solo se agrega al stack
+          stack.append(cadena[i])
+    ##si es un valor se agrega a los valores
+    else:
+      valores.append(cadena[i])
+    ##contador
+    i+=1
+
+  ##Si se termino el ciclo y aun hay cosas en el stack entonces las saca
+  if len(stack) !=0: 
+    for i in range (len(stack)):
+      valores.append(stack[-1])
+      stack.pop()
+  print(valores)
 
 
+infix_to_postfix("(a|b*)")
 
-##print(x)
-##print(evaluate(int(x[0]), int(x[2]), x[1]))
+expandir("(a|b*)?")
